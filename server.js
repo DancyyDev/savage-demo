f6c5db4355a88da87064e05fc23a0133bcc20220
Daniel Cha <cha.danieldev@gmail.com>
2022-05-30 18:34:14 -0400

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
+// variables of madules
+const express = require('express') //variables to require express
+const app = express() // calling on express
 const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient //geting mongodb
 
 var db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
+const password = encodeURIComponent("Kiwi2919");
+
+const url = `mongodb+srv://Dancyy:${password}@cluster0.cntrt.mongodb.net/?retryWrites=true&w=majority`;
 const dbName = "demo";
 
 app.listen(3000, () => {
@@ -18,10 +21,12 @@ app.listen(3000, () => {
     });
 });
 
+
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
   db.collection('messages').find().toArray((err, result) => {
@@ -31,14 +36,14 @@ app.get('/', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0}, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
   })
 })
 
-app.put('/messages', (req, res) => {
+app.put('/messages-up', (req, res) => {
   db.collection('messages')
   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
     $set: {
@@ -52,6 +57,22 @@ app.put('/messages', (req, res) => {
     res.send(result)
   })
 })
+
+app.put('/messages-down', (req, res) => {
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp -1 // added
+    }
+  }, {
+    sort: {_id: - 1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
 
 app.delete('/messages', (req, res) => {
   db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
